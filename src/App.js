dd82ceeceb8bd6cec8f8bd7a912f207e33ad421c
Jimbei <contrerasr2272@gmail.com>
2023-01-05 02:37:35 -0500

    About and Nav page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //it's not HTML; it's a language called JSX that can represent HTML in JavaScript. 
+    // Normally you can't add HTML to JavaScript, at least without making it a string.
+    // But it won't break the code in this case, because you're using webpack and React.
+    <div>
+      <Nav></Nav>
+        <main>
+        <About></About>
+      </main>
     </div>
   );
 }
